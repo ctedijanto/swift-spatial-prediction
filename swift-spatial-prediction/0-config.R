ec959cdef4abbe36929f2a1d412a562e6b720f90
ctedijanto <ctedijanto@gmail.com>
2022-02-28 15:08:52 -0800

    updating file structure

--- swift-spatial-prediction/0-config.R
+++ swift-spatial-prediction/0-config.R
@@ -1,53 +1,45 @@
 ### file description -----
 # Configuration file
-# for WUHA prediction
+# for SWIFT spatial prediction
 # Author: Christine Tedijanto christine.tedijanto@ucsf.edu
-# Last updated: 07/17/2021
+# Last updated: 02/28/2022
 ### ----------------------
-
-### load package environment -----
-#if (!require(renv)) install.packages('renv')
-#renv::restore()
 
 ### load packages -----
 # general
 library(tidyverse)
 library(devtools) # run packages from github
-library(here)
-here()
 
 # plots
 library(ggplot2)
-library(ggpubr)
+library(ggpubr) # stat_cor
 library(gridExtra) # grid.arrange
 library(corrplot)
 library(cowplot) # panel figures
 library(gt) # tables
 
-# api
-library(httr)
-library(jsonlite)
-
-# spatial packages
+# spatial
 library(rgee)
 library(sf)
 library(rgdal)
 library(lwgeom)
 library(raster)
-library(geojsonio) # for ee_extract
+library(geojsonio) # ee_extract
 library(exactextractr) # summarize raster values over polygonal areas
-library(blockCV) # spatial cross-validation
 
-# mapping packages
+# mapping
 library(osmdata) # open street map
 library(ggmap) # production version installed to address the issue described here: https://github.com/dkahle/ggmap/issues/287
 library(ggrepel)
-library(leaflet)
+library(ggsn) #for scale bars and north symbols
+library(rnaturalearth) # for africa country outlines
 
-# prediction packages
-library(origami) # testing package for cross-validation
+# prediction
+library(origami) # cross-validation folds
 library(sl3)
 library(R6) # allow for creation of own Lrnrs
+
+library(blockCV) # spatial cross-validation
 
 library(spaMM)
 library(nnls)
@@ -57,10 +49,6 @@ library(earth)
 library(randomForest)
 library(xgboost)
 
-### local paths -----
-data_path <- "../1-data"
-figure_path <- "../2-figures"
-
 ### global variables -----
 swift_crs <- 4326
 survey_list <- c(0,12,24,36)
@@ -68,13 +56,19 @@ age_group_list <- c("0-5y", "6-9y", "10+y")
 trach_ind <- c("prevalence_sero", "prevalence_pcr", "prevalence_clin")
 
 # create vectors containing start and end dates of each month in study
-# start with 2015 (year prior to baseline survey, which was conducted in December 2015)
+# start with 2015 (year prior to baseline survey, which was conducted at end of 2015 / start of 2016)
 start_dates <- as.character(seq(as.Date("2015-01-01"), length=12*4, by="months"))
 end_dates <- as.character(seq(as.Date("2015-02-01"), length=12*4, by="months")-1)
 
-# seropositives defined by following cutoffs
+# seropositivity thresholds were defined using ROC cutoff from reference samples
+# see Migchelsen, et al. Defining seropositivity thresholds for use in trachoma elimination studies. PLOS NTD 2017.
+# https://doi.org/10.1371/journal.pntd.0005230
 Pgp3_cutoff <- 1113
 Ct694_cutoff <- 337
 
 ### mini-functions -----
 `%ni%` <- Negate(`%in%`)
+
+### local paths -----
+data_path <- "data"
+output_path <- "output"
